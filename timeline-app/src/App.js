6c09fd688562f397131189afd40872cd253b907a
Louis Xavier Sheid III <louisxsheid@gmail.com>
2021-06-24 12:23:39 -0700

    add basic file structure

--- timeline-app/src/App.js
+++ timeline-app/src/App.js
@@ -1,9 +1,12 @@
 import './App.scss';
+// eslint-disable-next-line no-unused-vars
+import TimelinePage from './pages/timeline/TimelinePage';
 
 const App = () => {
     return (
         <div>
             App
+            <TimelinePage />
         </div>
     )
 }
---
+++ timeline-app/src/pages/timeline/TimelinePage.jsx
@@ -0,0 +1,11 @@
+import './TimelinePage.scss';
+
+const TimelinePage = () => {
+    return (
+        <div className="timeline-wrapper">
+            TimelinePage
+        </div>
+    )
+};
+
+export default TimelinePage;
