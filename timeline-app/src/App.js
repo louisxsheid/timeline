aea936b8ecbbbd67f4b30bbc0510b841611bf365
Louis Xavier Sheid III <louisxsheid@gmail.com>
2021-06-28 13:14:11 -0700

    add dow and current day

--- timeline-app/src/App.js
+++ timeline-app/src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 // eslint-disable-next-line no-unused-vars
-import { TimelinePage, DataShowCase } from "./pages/index";
+import { TimelinePage } from "./pages/index";
 import allData from "./pages/timeline/mock-data";
 const App = () => {
   return (
--- timeline-app/src/pages/timeline/DateRange/DateRangeColumn.jsx
+++ timeline-app/src/pages/timeline/DateRange/DateRangeColumn.jsx
@@ -1,7 +1,20 @@
 import "./DateRangeColumn.scss";
+import dayjs from "dayjs";
 
 const DateRangeColumn = ({ date }) => {
-  return <div className="date-range-wrapper">{date}</div>;
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const today = {
+    backgroundColor: "red"
+  }
+  const notToday = {
+    color: "white"
+  }
+  return (
+  <div className="date-range-wrapper" style={dayjs().format("MM/DD/YYYY") == date ? today : notToday}>
+    <div style={{color: "white"}}>{weekdays[dayjs(date).day()]}</div>
+    <div>{date}</div>
+  </div>
+  );
 };
 
 export default DateRangeColumn;
